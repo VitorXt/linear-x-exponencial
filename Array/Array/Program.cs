0e897e106dc3fb0cc790c524dc8efefac52d8778
VitorHRV <62753727+VitorXt@users.noreply.github.com>
2025-02-25 18:40:02 -0300

    Add files via upload

---
+++ Array/Array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        float[] A = new float[1000000];
+        for (int j = 0; j < A.Length; j++)
+        {
+            A[j] = j + 1;
+        }
+
+        float soma = 0;
+        for (int i = 0; i < A.Length; i++)
+        {
+            soma += A[i];
+            Console.WriteLine($"Soma parcial após adicionar A[{i}]: {soma}");
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine($"Soma final: {soma}");
+        Console.WriteLine($"Tempo de execução: {stopwatch.ElapsedMilliseconds} ms");
+        Console.ReadLine();
+    }
+}
---
+++ Exponencial/Exponencial/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+
+        int[] A = new int[100000];
+        Random daran = new Random();
+
+        for (int j = 0; j < A.Length; j++)
+        {
+            A[j] = daran.Next(1, 100000);
+        }
+
+        BigInteger soma = 1;
+        int i = A.Length - 1;  // Começa do último índice do array
+
+        // Repetir enquanto i for maior ou igual a 1
+        while (i >= 1)
+        {
+            soma = soma * A[i];  // Multiplica o número atual (A[i]) à soma
+            i--;  // Decrementa i
+
+        } // Continua até que i seja igual a 1
+
+        // Exibe o resultado final da soma
+        Console.WriteLine($"Soma final: {soma}");
+        Console.ReadLine();  // Para manter o console aberto
+    }
+}
